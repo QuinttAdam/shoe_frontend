e85e6857b7922e3415e02793041f9a2ae4a56eca
brittadam <britt.adam@hotmail.com>
2023-12-13 19:13:39 +0100

    chore: setup vue-router

--- src/main.js
+++ src/main.js
@@ -2,6 +2,24 @@ import { createApp } from 'vue'
 import './style.css'
 import App from './App.vue'
 
+//import components
+import Login from './components/Login.vue'
+import Orders from './components/Orders.vue'
+
+//import vue router
+import * as VueRouter from 'vue-router'
+
+const routes = [
+  { path: '/', component: Login },
+  { path: '/orders', component: Orders }
+]
+
+const router = VueRouter.createRouter({
+    history: VueRouter.createWebHashHistory(),
+    routes, 
+})
+
 const app = createApp(App)
+app.use(router)
 
 app.mount('#app')
