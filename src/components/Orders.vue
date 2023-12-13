dc9727ccb39c5356a49e613c612c416496a7c3c8
brittadam <britt.adam@hotmail.com>
2023-12-13 22:24:33 +0100

    feat: Add API request to fetch orders in Orders.vue

--- src/components/Orders.vue
+++ src/components/Orders.vue
@@ -1,4 +1,31 @@
 <script setup>
+import { ref, onMounted } from 'vue';
+// make onMounted available in the template
+onMounted(() => {
+  getOrders();
+});
+
+// make a fetch request to the API endpoint to get the orders
+const orders = ref([]);
+const error = ref(null);
+
+const getOrders= async ()=> {
+  const apiEndpoint = 'http://localhost:3000/api/v1/shoes';
+  try {
+    const response = await fetch(apiEndpoint);
+    const result = await response.json();
+    console.log(result);
+    if (result.status==='success') {
+      orders.value = result.data.orders;
+    } else {
+      error.value = 'Invalid email or password';
+    }
+  } catch (e) {
+    console.error('Error during login:', e);
+    error.value = 'An error occurred during login';
+  }
+};
+
 
 </script>
 
