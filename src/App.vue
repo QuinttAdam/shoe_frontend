b8b1c79eb5e45b3a48189924a8ae493e33011791
QuinttAdam <116001984+QuinttAdam@users.noreply.github.com>
2023-12-14 20:46:22 +0100

    refactor: adjust code for Orders component and create OrdersPage

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,4 @@
 <script setup>
-import { ref, onMounted } from 'vue';
-
 
 </script>
 
--- src/components/Orders.vue
+++ src/components/Orders.vue
@@ -1,38 +1,8 @@
 <script setup>
-import { ref, onMounted } from 'vue';
-
-// make onMounted available in the template
-onMounted(() => {
-  getOrders();
-});
-
-// make a fetch request to the API endpoint to get the orders
-const orders = ref([]);
-const error = ref(null);
-
-const getOrders = async () => {
-  const apiEndpoint = 'http://localhost:3000/api/v1/shoes';
-  try {
-    const response = await fetch(apiEndpoint);
-    const result = await response.json();
-    console.log(result);
-    if (result.status === 'success') {
-      orders.value = result.data;
-    } else {
-      error.value = 'Error retrieving orders';
-    }
-  } catch (e) {
-    console.error('Error retrieving orders:', e);
-    error.value = 'An error occurred while retrieving orders';
-  }
-};
+const props = defineProps(['order']);
 </script>
 
 <template>
-    <div>
-      <h2>ORDERS</h2>
-      <div v-if="error" class="error">{{ error }}</div>
-      <div v-for="order in orders" :key="order._id">
         <div>
           <h4>Ordernumber: {{ order._id }}</h4>
           <div>User: {{ order.user }}</div>
@@ -40,8 +10,6 @@ const getOrders = async () => {
           <div>Price: {{ order.price }}</div>
           <div>Size: {{ order.size }}</div>
         </div>
-      </div>
-    </div>
   </template>
   
 
