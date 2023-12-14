6fcf54cb410423b65508dbfb400c277c437fc4d8
QuinttAdam <116001984+QuinttAdam@users.noreply.github.com>
2023-12-14 21:13:59 +0100

    style: style orders

--- src/components/Orders.vue
+++ src/components/Orders.vue
@@ -3,16 +3,15 @@ const props = defineProps(['order']);
 </script>
 
 <template>
-        <div>
-          <h4>Ordernumber: {{ order._id }}</h4>
-          <div>User: {{ order.user }}</div>
-          <div>Status: {{ order.status }}</div>
-          <div>Price: {{ order.price }}</div>
-          <div>Size: {{ order.size }}</div>
-        </div>
-  </template>
-  
+  <div class="p-4 border-4 border-lime-400 bg-lime-100 rounded w-80 mr-4 mb-4">
+    <h4 class="text-lg font-bold mb-2">Ordernumber: {{ order._id }}</h4>
+    <div class="mb-2">User: {{ order.user }}</div>
+    <div class="mb-2">Status: {{ order.status }}</div>
+    <div class="mb-2">Price: {{ order.price }}</div>
+    <div>Size: {{ order.size }}</div>
+  </div>
+</template>
 
 <style scoped>
-/* Add your styling here */
+/* You can add more specific styles here if needed */
 </style>
--- src/pages/OrdersPage.vue
+++ src/pages/OrdersPage.vue
@@ -30,13 +30,13 @@ const getOrders = async () => {
 </script>
 
 <template>
-    <div>
-      <h2>ORDERS</h2>
-      <div v-if="error" class="error">{{ error }}</div>
-      <div v-for="order in orders" :key="order._id">
-        <Orders :order="order"/>
-      </div>
+  <h2 class="text-lime-400 text-2xl font-bold mb-4 ml-20 p-4">ORDERS</h2>
+    <div class="max-w-screen-md ml-20 p-4 flex flex-wrap">
+    <div v-if="error" class="text-red-500 mb-4">{{ error }}</div>
+    <div v-for="order in orders" :key="order._id">
+      <Orders :order="order"/>
     </div>
+  </div>
   </template>
   
 
