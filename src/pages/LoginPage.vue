78cf439cdd9735b20bb315694ef9d334e6b556fd
brittadam <britt.adam@hotmail.com>
2023-12-14 15:41:29 +0100

    Refactor: Add LoginPage.vue and update Login.vue component

---
+++ src/pages/LoginPage.vue
@@ -0,0 +1,14 @@
+<script setup>
+import Login from '../components/Login.vue';
+</script>
+
+<template>
+  <div class="flex flex-col justify-center items-center h-screen">
+    <h1 class="text-lime-400 font-bold text-2xl mb-4">Login</h1>
+    <Login />
+  </div>
+</template>
+
+<style scoped>
+/* Add your styling here */
+</style>
